081caf807f7d4a7271eb4023324db6e7606eb143
ismet kizgin <info@ismetkizgin.com.tr>
2022-06-29 15:53:20 +0300

    perf: remove simple controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import config from './utils/config';
 
 @Module({
@@ -11,7 +9,5 @@ import config from './utils/config';
       load: [config],
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
